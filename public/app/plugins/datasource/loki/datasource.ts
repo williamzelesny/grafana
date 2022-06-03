bff232697243394f2595cb420d8042b322c1308d
Gábor Farkas <gabor.farkas@gmail.com>
2022-06-03 08:49:16 +0200

    feature-flag removed

--- public/app/plugins/datasource/loki/datasource.ts
+++ public/app/plugins/datasource/loki/datasource.ts
@@ -117,7 +117,7 @@ export class LokiDatasource
     this.languageProvider = new LanguageProvider(this);
     const settingsData = instanceSettings.jsonData || {};
     this.maxLines = parseInt(settingsData.maxLines ?? '0', 10) || DEFAULT_MAX_LINES;
-    this.useBackendMode = config.featureToggles.lokiBackendMode ?? false;
+    this.useBackendMode = true;
   }
 
   _request(apiUrl: string, data?: any, options?: Partial<BackendSrvRequest>): Observable<Record<string, any>> {
